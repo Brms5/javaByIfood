d63717b4ed9a566ad40bbb483008a09a4edca083
Bruno <bruno.moreira@meta.com.br>
2022-11-17 19:51:34 -0300

    Java Singleton

---
+++ java/src/main/java/projeto_desafio/singleton/SingletonEager.java
@@ -0,0 +1,13 @@
+package projeto_desafio.singleton;
+
+public class SingletonEager {
+    private static SingletonEager instancia = new SingletonEager();
+
+    private SingletonEager() {
+        super();
+    }
+
+    public static SingletonEager getInstancia() {
+        return instancia;
+    }
+}
